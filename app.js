477b528a3b55662021a690ce82f4cae243fd2795
EvhenijGrebenuk <evgenijgrebenuk@gmail.com>
2018-10-20 16:55:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var str="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+
+   function search_simbol(s, a, b){
+     a = prompt("Enter the letter you need to find");
+     b = prompt("Enter a letter or character to replace");
+     var tmp="";
+     for(var i=0;i<s.length;i++){
+    if(s[i]===a)
+     {
+      tmp+=b;
+     }
+      else{
+        tmp+=s[i];
+       }
+     }
+    return tmp;
+    }
+    
+    console.log(search_simbol(str));
+  
+   
